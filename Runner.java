ee37a4a87eb1503bfddeaf723eada5fdd92c39ae
zeusofgods <zmarquez123@gmail.com>
2018-01-20 14:14:31 -0800

    Update Runner.java
    Added the RobotButtons

--- Runner.java
+++ Runner.java
@@ -3,7 +3,6 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,32 +17,33 @@ public class Runner {
 	static int width = 800;
 	static int height = 800;
 	static JFrame window = new JFrame("FRC PowerUp Simulation");
-	static JButton[] button = new JButton[8];
-	static JPanel panel = new JPanel();
+	static JButton[] actionButtons = new JButton[8];
+	static JButton[] robotButtons = new JButton[6];
+	static JPanel buttonPanel = new JPanel();
+	static JPanel robotPanel = new JPanel();
 	static JTextPane stats = new JTextPane();
 	static JTextPane action = new JTextPane();
-	static ArrayList<Robot> robots = new ArrayList<Robot>();
 
+	
 	public static void main(String args[]) {
 		Font f = new Font(Font.SANS_SERIF, 3, 75);
 		Font z = new Font(Font.SANS_SERIF, 3, 15);
-		Font p = new Font(Font.SANS_SERIF, 3, 30);
-		
-		Robot m = (new Robot((int)(Math.random()*10) + 10, (int)(Math.random()*10) + 10,
-				true, true));
-		
+
 		window.setLayout(null);
 		window.setVisible(true);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
 
-		panel.setLayout(new GridLayout(4, 2));
-		panel.setSize(width, height - (height / 4));
-		panel.setLocation(0, 200);
+		buttonPanel.setLayout(new GridLayout(4, 2));
+		buttonPanel.setSize(width, height - (height / 2));
+		buttonPanel.setLocation(0, 200);
+		
+		robotPanel.setLayout(new GridLayout(1,6));
+		robotPanel.setSize(width, height - ((height/4)*3));
+		robotPanel.setLocation(0, 600);
 
 		stats.setSize(width / 4, height / 4);
-		stats.setFont(p);
-		stats.setText(m.returnStats());
+		stats.setText("Stats:");
 
 		action.setSize((width - (width / 4) - 5), height / 4);
 		action.setLocation((width / 4) + 5, 0);
@@ -59,43 +59,60 @@ public class Runner {
 
 		window.setSize(width, height);
 		window.add(action);
-		window.add(panel);
+		window.add(buttonPanel);
 		window.add(stats);
-		
+		window.add(robotPanel);
+
 		for (int i = 0; i < 8; i++) {
-			button[i] = new JButton("" + (i + 1));
-			panel.add(button[i]);
-			button[i].addActionListener(new playListener());
-			button[i].setFont(z);
+			actionButtons[i] = new JButton("" + (i + 1));
+			buttonPanel.add(actionButtons[i]);
+			actionButtons[i].addActionListener(new playListener());
+			actionButtons[i].setFont(z);
 			switch (i) {
 			case 0:
-				button[i].setText("Place Cube onto Switch");
+				actionButtons[i].setText("Place Cube onto Switch");
 				break;
 			case 1:
-				button[i].setText("Place Cube onto Scale");
+				actionButtons[i].setText("Place Cube onto Scale");
 				break;
 			case 2:
-				button[i].setText("Toss Cube onto Switch (Close)");
+				actionButtons[i].setText("Toss Cube onto Switch (Close)");
 				break;
 			case 3:
-				button[i].setText("Toss Cube onto Scale (Close)");
+				actionButtons[i].setText("Toss Cube onto Scale (Close)");
 				break;
 			case 4:
-				button[i].setText("Toss Cube onto Switch (Far)");
+				actionButtons[i].setText("Toss Cube onto Switch (Far)");
 				break;
 			case 5:
-				button[i].setText("Toss Cube onto Scale (Far)");
+				actionButtons[i].setText("Toss Cube onto Scale (Far)");
 				break;
 			case 6:
-				button[i].setText("Single Climb");
+				actionButtons[i].setText("Single Climb");
 				break;
 			case 7:
-				button[i].setText("Dual Climb");
+				actionButtons[i].setText("Dual Climb");
 				break;
 
+			}}
+			
+			for(int i = 0; i < 6; i++){
+				robotButtons[i] = new JButton("Robot" + (i + 1));
+				robotPanel.add(robotButtons[i]);
+				robotButtons[i].addActionListener(new playListener());
+				robotButtons[i].setFont(z);
+				
+				
+				
+				if(i < 3){
+					robotButtons[i].setForeground(Color.red.darker());
+				}else{
+					robotButtons[i].setForeground(Color.blue.darker());
+				}
 			}
+			
 
-		}
+		
 		window.setBackground(Color.blue);
 
 	}
